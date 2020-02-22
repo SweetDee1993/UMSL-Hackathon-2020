4f81933f2fc906050d73ed1bcbd70ae7bf0e0e98
Dawnn Triplett <dawnnmaria1987@gmail.com>
2020-02-22 13:05:50 -0600

    initial code push

---
+++ core/routes.py
@@ -0,0 +1,22 @@
+from flask import request, render_template
+from . import app, db                         # Get from __init__
+
+from .models import Table                    # Get db models
+from .MessageHandler import MessageHandler   # Get simple message processor
+
+
+# Python imports
+
+
+msgHandler = MessageHandler()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    if request.method == 'GET':
+        return render_template('index.html',
+                                title=':::APP TITLE:::')
+
+    return render_template('error.html',
+                            title='Error!',
+                            message='Must use GET request type...')
