b7c0564b002a640c694def75f9355a26eb656c1d
blktx <blktx@mail.umsl.edu>
2020-02-22 21:46:11 -0600

    added heart-disease route

--- Reaper Creeper/core/routes.py
+++ Reaper Creeper/core/routes.py
@@ -26,7 +26,30 @@ def root():
 def diabetes():
     if request.method == 'GET':
         return render_template('diabetes.html',
-                                title=':::Reaper Creeper2:::')
+                                title=':::Diabetes:::')
+
+    return render_template('error.html',
+                            title='Error!',
+                            message='Must use GET request type...')
+
+
+@app.route('/heart-disease', methods=['GET', 'POST'])
+def heartDisease():
+    if request.method == 'GET':
+        return render_template('heart-disease.html',
+                                title=':::Heart Disease:::')
+
+    return render_template('error.html',
+                            title='Error!',
+                            message='Must use GET request type...')
+
+
+@app.route('/heart-disease-analysis', methods=['GET', 'POST'])
+def heartDiseaseAnalysis():
+    if request.method == 'POST':
+        age    = request.values['age'].strip()
+        gender = request.values['gender'].strip()
+        return "Age: " + age + " Gender: " +  gender
 
     return render_template('error.html',
                             title='Error!',
